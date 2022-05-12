5926d1197634e451a99850c7c115efec031a7afe
watanabe erika <erika.watanabe@rakus-partners.co.jp>
2022-05-12 11:48:29 +0900

    exam04

---
+++ src/main/java/com/example/form/UserForm.java
@@ -0,0 +1,37 @@
+package com.example.form;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class UserForm {
+	@NotBlank(message="名前は必須です")
+	private String name;
+	
+	@NotBlank(message="年齢は必須です")
+	private Integer age;
+	
+	@Size(min=1,max=127,message="コメントは１文字以上127文字以内で記載してください")
+	private String comment;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	
+	
+}
