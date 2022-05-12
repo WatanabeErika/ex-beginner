568e2b8a1553e0654d5771983b59e90de8f77f69
watanabe erika <erika.watanabe@rakus-partners.co.jp>
2022-05-12 13:40:38 +0900

    exam05

---
+++ src/main/java/com/example/controller/Exam05Controller.java
@@ -0,0 +1,39 @@
+package com.example.controller;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.Member;
+import com.example.repository.MemberRepository;
+
+
+
+@Controller
+@RequestMapping("/exam05")
+public class Exam05Controller {
+	@Autowired
+	private MemberRepository repository;
+	
+	@RequestMapping("")
+	public String index() {
+		return "exam05";
+	}
+	
+	
+	@RequestMapping("/result")
+	public String result(Model model) {
+		Member member=new Member();
+		
+		repository.findName("ロー");
+		
+		model.addAttribute("member", member);
+		return "exam05-result";
+	
+}
+	
+	
+}
---
+++ src/main/java/com/example/repository/MemberRepository.java
@@ -0,0 +1,32 @@
+package com.example.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.example.domain.Member;
+
+@Repository
+public class MemberRepository {
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+
+	
+	private RowMapper<Member> ROW_MAPPER_MEMBER=(rs,i)->{
+		Member member=new Member();
+		member.setId(rs.getInt("id"));
+		member.setName(rs.getString("name"));
+		member.setAge(rs.getInt("age"));
+		member.setDepId(rs.getInt("dep_id"));
+		return member;
+	};
+	
+	public List<Member> findName(String name){
+		String sql="SELECT name FROM members WHERE name LIKE %:name%";
+		
+		return template.query(sql, ROW_MAPPER_MEMBER);
+	}
+}
