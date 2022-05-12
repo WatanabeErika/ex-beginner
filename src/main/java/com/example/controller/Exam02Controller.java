98aae796457758296fbd81862cd5604366378d97
watanabe erika <erika.watanabe@rakus-partners.co.jp>
2022-05-12 10:23:18 +0900

    exam023

--- src/main/java/com/example/controller/Exam02Controller.java
+++ src/main/java/com/example/controller/Exam02Controller.java
@@ -1,12 +1,18 @@
 package com.example.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/exam02")
 public class Exam02Controller {
+	
+	@Autowired
+	private HttpSession session;
+	
 	@RequestMapping("")
 	public String index() {
 		return "exam02";
@@ -14,11 +20,11 @@ public class Exam02Controller {
 	
 	
 	@RequestMapping("/result1")
-	public String result1(int num1,int num2,Model model) {
+	public String result1(int num1,int num2) {
 		int ans=num1+num2;
-		model.addAttribute("num1", num1);
-		model.addAttribute("num2", num2);
-		model.addAttribute("ans", ans);
+		session.setAttribute("num1", num1);
+		session.setAttribute("num2", num2);
+		session.setAttribute("ans", ans);
 		return "exam02-result";
 	}
 	
