74e1f88a03622b6be8e0ddea694fd617587b8846
watanabe erika <erika.watanabe@rakus-partners.co.jp>
2022-05-12 12:30:30 +0900

    exam048

--- src/main/java/com/example/controller/Exam04Controller.java
+++ src/main/java/com/example/controller/Exam04Controller.java
@@ -3,14 +3,15 @@ package com.example.controller;
 
 
 
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
+
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 import com.example.domain.User;
 import com.example.form.UserForm;
@@ -29,17 +30,22 @@ public class Exam04Controller {
 	}
 	
 	
-	@RequestMapping("/create")
-	public String create(@Validated UserForm form,BindingResult result,RedirectAttributes redirectAttributes) {
+	@RequestMapping("/result")
+	public String result(@Validated UserForm userForm,BindingResult result,Model model) {
 		if(result.hasErrors()) {
 			return index();
 		}
 		
 		User user=new User();
+		user.setName(userForm.getName());
+		user.setAge(userForm.getAge());
+		user.setComment(user.getComment());
 		
-		BeanUtils.copyProperties(form,user);
+		model.addAttribute("name", userForm.getName());
+		model.addAttribute("age", userForm.getAge());
+		model.addAttribute("comment", userForm.getComment());
 		
-		redirectAttributes.addFlashAttribute("user",user);
+		
 		
 		return "exam04-result";
 	
